ab9ebafa3f18b1fe60555165d60f78b6ffc87e8f
Gandobilis <lashadeveloper@gmail.com>
2024-09-20 23:50:16 +0400

    DropDown.vue implemented.

---
+++ src/components/DropDown.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  options: {
+    type: Array,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+})
+
+const model = defineModel()
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-1.5 text-[#021526] text-sm">
+    <label for="dropdown" class="font-medium" v-text="title"/>
+
+    <div class="relative">
+      <select id="dropdown" v-model="model"
+              class="cursor-pointer w-full appearance-none bg-white border border-[#808A93] p-2.5 rounded-md focus:outline-none">
+        <option disabled selected v-text="`აირჩიეთ ${title}`"/>
+        <option v-for="(option, index) in options" :key="index" :value="option" v-text="option.name"/>
+      </select>
+
+      <div class="absolute inset-y-0 right-0 flex items-center px-2">
+        <img src="/src/assets/icons/caret.svg" alt="caret"/>
+      </div>
+    </div>
+  </div>
+</template>
